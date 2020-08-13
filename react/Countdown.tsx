53b98898c4622b7a16c4ba81744e95c11a513ae5
Vinicius Tito <viniciustito@gmail.com>
2020-08-13 16:32:20 -0300

    issue05

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { TimeSplit } from "./typings/global";
 import { tick } from "./utils/time";
+import { useCssHandles } from "vtex.css-handles";
 
 interface CountdownProps {
   targetDate: string;
 }
+const CSS_HANDLES = ["countdown"];
 const DEFAULT_TARGET_DATE = new Date("2020-06-25").toISOString();
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
@@ -15,9 +17,12 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     minutes: "00",
     seconds: "00",
   });
+  const handles = useCssHandles(CSS_HANDLES);
   tick(targetDate, setTime);
   return (
-    <div>
+    <div
+      className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}
+    >
       <h1>{targetDate}</h1>
       <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
